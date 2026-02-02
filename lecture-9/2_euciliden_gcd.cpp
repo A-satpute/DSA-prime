8bffeca5e796eb2b22c851e1e207a963c642c0e2
A-satpute <ashishsatpute068@gmail.com>
2026-02-02 22:30:31 +0530

    notes

---
+++ lecture-9/2_euciliden_gcd.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a,int b)
+{
+    while(b!=0)
+    {
+        int t=a%b;
+        a=b;
+        b=t;
+    }
+    return a;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b)<<endl;
+
+    //without function
+    while(b!=0)
+    {
+        int t=a%b;
+        a=b;
+        b=t;
+    }
+    cout<<gcd(a,b)<<endl;
+
+    return 0;
+}
---
+++ lecture-9/3_gcd_stl.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>  //this is a package of algorithm
+
+using namespace std;
+
+int main()
+{
+    int a=5;
+    int b=10;
+    cout<<__gcd(a,b);  //__gcd()  they are inbiuld gcd function
+
+    return 0;
+}
