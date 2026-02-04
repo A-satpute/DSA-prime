67d15ffe1697f43f9c10adc4f1f66097a36a6c7b
A-satpute <ashishsatpute068@gmail.com>
2026-02-04 23:18:45 +0530

    notes

--- lecture-11/4_decimal_to_binary.cpp
+++ lecture-11/4_decimal_to_binary.cpp
@@ -6,27 +6,24 @@ int rev(int n)
     int rev=0;
     while(n!=0)
     {
-        int k=n%2;
-        bn=bn*10+k;
-        n=n/2;
+        int k=n%10;
+        rev=rev*10+k;
+        n=n/10;
     }
     //recursion
-    return fibbo(n-1)+fibbo(n-2);
+    return rev;
 }
 int main()
 {
     int n;
     cin>>n;
-    int g=0;
-    for(int i=0;i<=n;i++)
+    int bn=1;
+    while(n>0)
     {
-        for(int j=0;j<=i;j++)
-        {
-           cout<<fibbo(g)<<"  "; 
-           g++; 
-        }
-       
+        int k=n%2;
+        bn=bn*10+k;
+        n=n/2;
     }
-    
+    cout<<rev(bn)/10<<endl;
     return 0;
 }
