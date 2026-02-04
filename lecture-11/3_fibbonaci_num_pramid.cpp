d50d1c8221e7a374e97e681a1327ead960218a6c
A-satpute <ashishsatpute068@gmail.com>
2026-02-04 22:59:57 +0530

    notes

--- lecture-11/3_fibbonaci_num_pramid.cpp
+++ lecture-11/3_fibbonaci_num_pramid.cpp
@@ -2,24 +2,24 @@
 #include<cmath>
 using namespace std;
 
-int fibbo(int n)
+int fibbo(int g)
 {
     //base case
-    if(n==0 or n==1)
+    if(g==0 || g==1)
     {
-        return n;
+        return g;
     }
     //recursion
-    return fibbo(n-1)+fibbo(n-2);
+    return fibbo(g-1)+fibbo(g-2);
 }
 int main()
 {
     int n;
     cin>>n;
     int g=0;
-    for(int i=0;i<=n;i++)
+    for(int i=1;i<=n;i++)
     {
-        for(int j=0;j<=i;j++)
+        for(int j=1;j<=i;j++)
         {
            cout<<fibbo(g)<<"  "; 
            g++; 
