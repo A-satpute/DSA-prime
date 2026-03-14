1c4936157a395b008c588867ca820ed161f16676
A-satpute <ashishsatpute068@gmail.com>
2026-03-14 23:23:50 +0530

    notes

---
+++ lecture-19/String_basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    // 1. length/size function = both are use for check the length of string .
+    string s="ashish";
+    cout<<s.length()<<endl;
+    cout<<s.size()<<endl;
+
+    return 0;
+}
