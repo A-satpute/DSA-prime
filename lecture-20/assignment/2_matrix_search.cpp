a6e58493abeb7446d1021c75b70d7bd7b8efcdfa
A-satpute <ashishsatpute068@gmail.com>
2026-03-15 22:46:09 +0530

    assignment

---
+++ lecture-20/assignment/2_matrix_search.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+
+    int arr[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[n][m];
+        }
+    }
+
+    int target;
+    cin>>target;
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+         for(int j=0;j<m;j++)
+        {
+            if(arr[i][j]==target)
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    if(count>0)
+    {
+        cout<<0;
+    }
+    else{
+        cout<<1;
+    }
+}
